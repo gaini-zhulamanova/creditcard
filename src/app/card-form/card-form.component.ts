5c5e96117f8b967669d520cc5970d40f99757960
gaini-zhulamanova <gaini.zhulamanova@gmail.com>
2020-05-19 15:33:57 +0200

    add name field and validation

---
+++ src/app/card-form/card-form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-card-form',
+  templateUrl: './card-form.component.html',
+  styleUrls: ['./card-form.component.css']
+})
+export class CardFormComponent implements OnInit {
+
+  cardForm = new FormGroup({
+    // '' for default values
+    name: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3)
+    ])
+  });
+
+  constructor() {
+    console.log(this.cardForm.get('name'));
+   }
+
+  ngOnInit(): void {
+  }
+
+}
